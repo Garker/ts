34f580228e22f57ded60a0cc93ff27ffc12a230b
Garker <200571164@qq.com>
2022-01-06 21:00:15 +0800

    feat

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,13 @@
 import React from 'react';
-import Board from './Component/BoardComp';
-import { ChessType } from './type/enmus';
+import { GameComp } from './Component/GameComp';
 
-const chesses:ChessType[] = [
-  ChessType.none,
-  ChessType.none,
-  ChessType.none,
-  ChessType.red,
-  ChessType.red,
-  ChessType.red,
-  ChessType.black,
-  ChessType.black,
-  ChessType.black,
-]
-export default class App extends React.Component{
+export class App extends React.Component {
 
-  render() {
-    return (
-      <div>
-       <Board chesses={chesses} onClick = {i => {
-         console.log(i)
-       }}/>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <GameComp />
+            </div>
+        )
+    }
 }
